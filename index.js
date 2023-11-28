b79a68c2bbd9d5cbdfa3ec6f90f44de039bb167e
sami5671 <samialam5671@gmail.com>
2023-11-28 09:17:58 +0600

    9. addded the admin role and moderator role api

--- index.js
+++ index.js
@@ -98,7 +98,41 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
-
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    app.delete("/user/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+    // ---for admin role ---
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    // ---for moderator role ---
+    app.patch("/users/moderator/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "moderator",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    // =================================================================
     app.post("/userAddedProduct", async (req, res) => {
       const item = req.body;
       const result = await userAddedCollection.insertOne(item);
